f52be404d92d008a111886ed0ae4f9474a867ec6
RemoteCoder <shah.duet@gmail.com>
2014-05-28 03:21:37 +0600

    SECRET_TOKEN set to environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,9 +5,15 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
-  raise 'The SECRET_TOKEN environment variable is not set.\n
-  To generate it, run "rake secret", then set it with "heroku config:set SECRET_TOKEN=the_token_you_generated"'
+#if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+#  raise 'The SECRET_TOKEN environment variable is not set.\n
+#  To generate it, run "rake secret", then set it with "heroku config:set SECRET_TOKEN=the_token_you_generated"'
+#end
+
+if Rails.env.production?
+  ENV['SECRET_TOKEN'] = 'a80e1ecdcee5f9493d2a59eb4529521a08a9b2b88298fa9733db6935758258ee2d528286acf5750ef4ce4634b1382b83cb02ec634c2514d700a23b566bcc9de3'
+else
+  fail 'Production Mode running Failed'
 end
 
 Hsa::Application.config.secret_token = ENV['SECRET_TOKEN'] || '7f5df2af31826553ca56e75c2890e802165249fc528a0c343fad14c604750be43c073ec41bad68e935d64f26a878d48a379d649e4ba2f7ff87fc08826924fce3'

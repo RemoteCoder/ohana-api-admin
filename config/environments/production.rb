5e63e88c4f512d17b8c3b26a65f0299c8c084e06
RemoteCoder <shah.duet@gmail.com>
2014-06-05 23:15:40 +0600

    Action mailer set to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,20 +52,33 @@ Hsa::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.default_url_options = { :host => ENV["MAILER_URL"] }
+  #config.action_mailer.default_url_options = { :host => ENV["MAILER_URL"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
+  #config.action_mailer.smtp_settings = {
+  #  :port =>           '587',
+  #  :address =>        'smtp.mandrillapp.com',
+  #  :user_name =>      ENV['MANDRILL_USERNAME'],
+  #  :password =>       ENV['MANDRILL_APIKEY'],
+  #  :domain =>         'heroku.com',
+  #  :authentication => :plain
+  #}
+
+  config.action_mailer.default_url_options = { :host => 'tts.callforamerica.com',:protocol => 'http', :port => 8081 }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :authentication => :login,
+      :user_name => "shahc4a",
+      :password => "shahcall4a",
+      :enable_starttls_auto => true,
+      :openssl_verify_mode => 'none'
   }
 
   # Enable threaded mode
